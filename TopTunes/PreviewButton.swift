5e6303852fd04d7312007320dddce1ed4bd22c1c
David Lobo <david@davidlobo.co.uk>
2016-03-26 19:18:33 +0000

    loading the iTunes images from cache

--- TopTunes/PreviewButton.swift
+++ TopTunes/PreviewButton.swift
@@ -72,30 +72,3 @@ class PreviewButton: UIButton {
         }
     }
 }
-
-extension PreviewButton {
-    
-    //*****************************************************************
-    // Load image async and replace the placeholder image when done
-    //*****************************************************************
-    
-    func loadImageWithURL(url: NSURL) -> NSURLSessionDownloadTask {
-        let session = NSURLSession.sharedSession()
-        let downloadTask = session.downloadTaskWithURL(
-            url, completionHandler: { [weak self] url, response, error in
-    
-                if error == nil, let url = url,
-                    data = NSData(contentsOfURL: url), image = UIImage(data: data) {
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        if let strongSelf = self {
-                            strongSelf.playImageView.image = image
-                        }
-                    }
-                }
-            })
-        
-        downloadTask.resume()
-        return downloadTask
-    }
-}
--- TopTunes/TrackCell.swift
+++ TopTunes/TrackCell.swift
@@ -16,6 +16,7 @@ protocol TrackCellDelegate: class {
 
 class TrackCell: UITableViewCell {
     
+    var imageCache = NSCache()
     weak var delegate: TrackCellDelegate?
     var downloadTask: NSURLSessionDownloadTask?
     var previewURL: NSURL!
@@ -25,31 +26,63 @@ class TrackCell: UITableViewCell {
     @IBOutlet weak var artworkImageView: UIImageView!
     @IBOutlet weak var artworkImageButton: PreviewButton!
     @IBOutlet weak var rankLabel: UILabel!
-
+    
     func configureForSearchResult(searchResult: Track, rank: Int, currentlyPlaying: Bool) {
         
         rankLabel.text = String(rank)
         nameLabel.text = searchResult.name
-    
+        
         if searchResult.artistName.isEmpty {
             artistNameLabel.text = NSLocalizedString("Unknown", comment: "Empty field value: Artist name")
         } else {
             artistNameLabel.text = searchResult.artistName
         }
         
-        if let url = NSURL(string: searchResult.artworkURL60) {
-            downloadTask = artworkImageButton.loadImageWithURL(url)
-        }
-        
         if searchResult.previewURL != "" {
             previewURL = NSURL(string: searchResult.previewURL)
         }
+        
+        loadImageWithURL(searchResult)
         
         artworkImageButton.changeState(currentlyPlaying ? PreviewButton.State.Playing : PreviewButton.State.Stopped, withAnimation: false)
     }
     
     func setPreviewButtonState(newState: PreviewButton.State, withAnimation: Bool = true) {
         artworkImageButton.changeState(newState, withAnimation: withAnimation)
+    }
+    
+    //*****************************************************************
+    // Load the image from cache or async from remote if needed
+    //*****************************************************************
+    
+    func loadImageWithURL(searchResult: Track) {
+        if let imageURL = NSURL(string: searchResult.artworkURL60) {
+            if let image = imageCache.objectForKey(searchResult.artworkURL60) as? UIImage {
+                
+                // set the ImageView image from the cache
+                artworkImageButton.playImageView.image = image
+            } else {
+                let downloadTask = NSURLSession.sharedSession().dataTaskWithURL(imageURL, completionHandler: { data, response, error in
+                    
+                    if error != nil {
+                        print(error)
+                        return
+                    }
+                    
+                    let image = UIImage(data: data!)
+                    
+                    // set the image to cache
+                    self.imageCache.setObject(image!, forKey: searchResult.artworkURL60)
+                    
+                    dispatch_async(dispatch_get_main_queue()) {
+                        
+                        self.artworkImageButton.playImageView.image = image
+                    }
+                })
+                
+                downloadTask.resume()
+            }
+        }
     }
     
     @IBAction func previewButtonTapped() {
